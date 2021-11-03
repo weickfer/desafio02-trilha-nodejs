370b56459329fc6a77ffa86745e5491c6a57cf30
Weickmam Ferreira <54916834+weickfer@users.noreply.github.com>
2021-11-03 00:34:29 -0300

    Challenge Solution

--- src/index.js
+++ src/index.js
@@ -10,19 +10,85 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not exists!'
+    })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (!user.pro && user.todos.length < 10) {
+    return next()
+  }
+
+  if (user.pro) {
+    return next()
+  }
+
+  return response.status(403).json({
+    error: 'User not are available to create new TODO!'
+  })
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not exists!'
+    })
+  }
+
+  const todoIDIsUUID = validate(id)
+
+  if (!todoIDIsUUID) {
+    return response.status(400).json({
+      error: 'Todo Id not is UUID.'
+    })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Todo not exists!'
+    })
+  }
+
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({
+      error: 'User not exists!'
+    })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
@@ -47,7 +113,10 @@ app.post('/users', (request, response) => {
   return response.status(201).json(user);
 });
 
-app.get('/users/:id', findUserById, (request, response) => {
+app.get(
+  '/users/:id', 
+  findUserById, 
+  (request, response) => {
   const { user } = request;
 
   return response.json(user);
